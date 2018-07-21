c3f1fe16db2d5866d08ee5ee7f561aa8b3952f06
Sarah <sarah.wolf32@gmail.com>
2018-07-21 14:27:53 +0200

    cleanup

---
+++ discriminator.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+class Discriminator():
+
+    def prob_real(self):
+        '''
+        Computes the probability that the given image patch is real.
+        '''
+        print "coming soon!"
